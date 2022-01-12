19f8b3377e3291d5eb441e0d54dcb0546003398c
Milan Bogaert <milanbbogaert@gmail.com>
2022-01-12 18:40:40 +0100

    no comment

--- Christmas Three EXEC.sh
+++ Christmas Three EXEC.sh
@@ -37,56 +37,45 @@ if [ $# -ne 1 ];then
     exit 4
 fi
 
-#move #i directories up.
 up() {
     i=${1:-1}
     while ((i--)); do
         cd ..
     done
 }
-
-eerstebestand=$(echo $1 | cut -d '/' -f3)
+bestand2=$(echo $1 | cut -d '/' -f3)
 bestand=$1
 
 COUNTER=0
 while [[ $COUNTER -lt $s ]];do
-    if [[ $COUNTER -eq 0 ]];then
-        newdir=$(dirname $bestand)
-        cat $bestand | head -3 | tail -2
-        cd $newdir
-        head -$n $bestand2 > file.txt
-    else
-        head -$n $newbestand > file.txt
-    fi
-    
-    token=$(cat file.txt | head -1 | cut -d ' ' -f2)
-    newbestand=$(cat file.txt | head -1 | cut -d ' ' -f3)
-    rm file.txt
+if [[ $COUNTER -eq 0 ]];then
+    newdir=$(dirname $bestand)
+    cat $bestand | head -3 | tail -2
+    cd $newdir
+    head -$n $bestand2 > file.txt
+else
+    head -$n $newbestand > file.txt
+fi
 
 
-    if [[ $token =~ ^[1-9]$ ]];then
+token=$(cat file.txt | head -1 | cut -d ' ' -f2)
+newbestand=$(cat file.txt | head -1 | cut -d ' ' -f3)
+rm file.txt
+if [[ $token =~ ^[1-9]$ ]];then
     up $token
-    elif [[ $token =~ ^0$ ]];then
+elif [[ $token =~ ^0$ ]];then
         cd .
+else
+    if [[ ! -d "$token" ]];then
+        exit 0
     else
-        if [[ ! -d "$token" ]];then
-            exit 0
-        else
-            cd ./$token
-        fi
+        cd ./$token
     fi
+fi
 
-    if [[ $m -eq 1 ]];then
-        cat $newbestand | head -3 | tail -2 | rev
-    else
-        cat $newbestand | head -3 | tail -2
-    fi
-
-    let COUNTER=COUNTER+1
+cat $newbestand | head -3 | tail -2
+let COUNTER=COUNTER+1
 done
-exit 0
-
-
 
 
 
